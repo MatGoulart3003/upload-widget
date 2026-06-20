6327e9589c1a6b4d7218b6ba8fb129c78d39392d
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2026-06-20 16:57:51 -0300

    feat: update UploadWidget stylesheet background color to red

--- src/components/UploadWidget/UploadWidget.tsx
+++ src/components/UploadWidget/UploadWidget.tsx
@@ -9,7 +9,7 @@ import { useUploadWidget } from './hooks/useUploadWidget'
 
 const stylesheet = {
   container:
-    'bg-zinc-900 overflow-hidden w-90 rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,var(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),oklch(55.17%_0.014_285.95/.48)_80%,var(--color-indigo-500)_86%,var(--color-indigo-300)_90%,var(--color-indigo-500)_94%,oklch(55.17%_0.014_285.95/.48))_border-box]',
+    'bg-red-900 overflow-hidden w-90 rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,var(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),oklch(55.17%_0.014_285.95/.48)_80%,var(--color-indigo-500)_86%,var(--color-indigo-300)_90%,var(--color-indigo-500)_94%,oklch(55.17%_0.014_285.95/.48))_border-box]',
   content: 'flex flex-col gap-4 py-3',
   divider: 'h-px bg-zinc-800 border-t border-black/50 box-content',
 }
